e0500b995fabf13a43226d7af48cf31f8fc56d56
roman.turubayev@econophysica.com <turubaevrr@dev002.local>
2026-02-13 09:13:14 +0700

    Fixed text

--- app/src/main/java/dev/catandbunny/cloudbuddy/MainActivity.kt
+++ app/src/main/java/dev/catandbunny/cloudbuddy/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello, $name! Mr",
+        text = "Hello, $name!",
         modifier = modifier,
     )
 }
